52ccaaec9c3127c901ba00e4aa3c4de365c9d672
dcuencabla001 <45784651+dcuencabla001@users.noreply.github.com>
2021-12-16 12:41:41 +0100

    Add files via upload

---
+++ uploadweb2.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+st.image(image='logoEroski.png')
+
+st.title('Carga Diaria Plataforma')
+st.header('Subir el archivo en formato CSV con la carga diaria de la plataforma')
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    #df.to_csv('out.csv')
+    st.header("archivo guardado correctamente")
