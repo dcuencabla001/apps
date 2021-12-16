16180a18ba9fec01f100a244881f526ceeca264e
dcuencabla001 <45784651+dcuencabla001@users.noreply.github.com>
2021-12-16 12:28:25 +0100

    Add files via upload

--- uploadweb.py
+++ uploadweb.py
@@ -1,6 +1,21 @@
 import streamlit as st
 import pandas as pd
 
+from azure.storage.blob import BlobServiceClient
+
+StorageAccountName1 = "storagealisamiento"
+StorageAccountKey1 = "QSrEesxEZvyPyYKOtOzHSOsYueT9Zgv7c0tYT953aRBmSMGCBc8fDTrd0O96c/LeEspOTMVomTdJCgWjTVJFjg=="
+
+blobService = BlobServiceClient(account_url="https://" + StorageAccountName1 + ".blob.core.windows.net", credential=StorageAccountKey1)
+
+def write_dataframe_to_csv_blob(df, container, path, sep, decimal, blob_service, header=True):
+    csv_string = df.to_csv(index=False, header=header, sep = sep, decimal = decimal, date_format='%Y-%m-%d')
+    try:
+        result = blob_service.get_blob_client(container, path).upload_blob(csv_string, blob_type="BlockBlob", overwrite=True)
+    except:
+        result = blob_service.get_blob_client(container, path).upload_blob(csv_string, blob_type="BlockBlob", overwrite=True)
+    return result
+
 st.image(image='logoEroski.png')
 
 st.title('Carga Diaria Plataforma')
@@ -12,4 +27,4 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     #df.to_csv('out.csv')
     st.header("archivo guardado correctamente")
-    #st.write("archivo guardado correctamente")
+    write_dataframe_to_csv_blob(df, 'datauser', '4d_nueva_planificacion_plataforma.csv', ";", ".", blobService, header=True)
